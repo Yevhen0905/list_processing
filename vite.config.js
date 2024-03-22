9efd9a338a733b047f9ccbeb50669098f4ac0f2a
Vittcenko <evgenav83@gmail.com>
2024-03-22 17:22:40 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/list_people/',
+  base: '/list_processing/',
   plugins: [vue()],
   resolve: {
     alias: {
