dcddb4db639b05485b789d8d472f85708f80c554
Vittcenko <evgenav83@gmail.com>
2024-05-15 16:27:05 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import MainPage from '../views/MainPage.vue';
-// import ListPeople from '../views/ListPeople.vue';
-// import ListPreview from '../views/ListPreview.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,19 +9,6 @@ const router = createRouter({
       name: 'main',
       component: MainPage
     }
-    // {
-    //   path: '/list',
-    //   name: 'list',
-    //   component: ListPeople
-    // },
-    // {
-    //   path: '/preview',
-    //   name: 'preview',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: ListPreview
-    // }
   ]
 });
 
